2cf1d4e084a48fc0641417e571011b99ab724afd
Nick Matantsev <nick.matantsev@gmail.com>
2020-12-10 23:33:02 -0500

    Add comment in demo code

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import { useSafeCallback } from './SafeCallback';
 import './index.css';
 
 const App: React.FC = () => {
+  // this action is only possible to trigger while this component is mounted;
+  // there is no direct way to invoke it, which means fewer accidents;
+  // instead, one uses an attach helper below to "plug in" a button/link/etc
   const hiTrigger = useSafeCallback(() => {
     window.alert('oh hi there!');
   });
